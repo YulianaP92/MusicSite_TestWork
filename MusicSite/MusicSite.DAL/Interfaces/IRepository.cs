fc47c267cfa519046b2c56714fdcc7123a98645f
yulia <polushka.yulia@gmail.com>
2019-07-09 16:02:34 +0300

    c2

--- MusicSite/MusicSite.DAL/Interfaces/IRepository.cs
+++ MusicSite/MusicSite.DAL/Interfaces/IRepository.cs
@@ -10,8 +10,5 @@ namespace MusicSite.DAL.Interfaces
     {
         IEnumerable<T> GetAll();
         T Get(int id);
-        void Create(T item);
-        void Update(T item);
-        void Delete(int id);
     }
 }
--- MusicSite/MusicSite.DAL/Repositories/SongRepository.cs
+++ MusicSite/MusicSite.DAL/Repositories/SongRepository.cs
@@ -18,22 +18,6 @@ namespace MusicSite.DAL.Repositories
         {
             this.db = db;
         }
-        public void Create(Song song)
-        {
-            db.Songs.Add(song);
-            db.Entry(song).State = EntityState.Added;
-            db.SaveChanges();
-            
-        }
-
-        public void Delete(int id)
-        {
-            var song = db.Songs.Find(id);
-            if (song != null)
-                db.Songs.Remove(song);
-            db.SaveChanges();
-        }
-
         public Song Get(int id)
         {
             return db.Songs.Find(id);
@@ -42,17 +26,6 @@ namespace MusicSite.DAL.Repositories
         public IEnumerable<Song> GetAll()
         {
             return db.Songs;
-        }
-
-        public void Update(Song songNew)
-        {
-            var modifiedSongInDb = db.Songs.Find(songNew.Id);
-            if (modifiedSongInDb != null)
-            {
-                db.Entry(modifiedSongInDb).CurrentValues.SetValues(songNew);
-                db.Entry(modifiedSongInDb).State = EntityState.Modified;
-                db.SaveChanges();
-            }
         }
     }
 }
