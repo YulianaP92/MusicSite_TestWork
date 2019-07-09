cf70c418f3a678fa5e294bfb8431ad73f981ec83
yulia <polushka.yulia@gmail.com>
2019-07-09 15:32:26 +0300

    Commit

--- MusicSite/MusicSite.DAL/Interfaces/IRepository.cs
+++ MusicSite/MusicSite.DAL/Interfaces/IRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace MusicSite.DAL.Interfaces
 {
-    interface IRepository
+    interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
     }
 }
---
+++ MusicSite/MusicSite.DAL/Repositories/SongRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicSite.DAL.EF;
+using MusicSite.DAL.Entities;
+using MusicSite.DAL.Interfaces;
+
+namespace MusicSite.DAL.Repositories
+{
+    public class SongRepository : IDisposable, IRepository<Song>
+    {
+        private readonly SongContext db=new SongContext(); 
+        public void Create(Song item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Song Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Song> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Song item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
