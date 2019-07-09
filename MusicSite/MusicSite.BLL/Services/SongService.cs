045fc786cd98c3204b5dd922e1634117b336d2fa
yulia <polushka.yulia@gmail.com>
2019-07-09 16:15:29 +0300

    16_15

---
+++ MusicSite/MusicSite.BLL/Services/SongService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicSite.BLL.DTO;
+using MusicSite.BLL.Interfaces;
+using MusicSite.DAL.EF;
+
+namespace MusicSite.BLL.Services
+{
+    public class SongService : IService<SongDto>
+    {
+        private SongContext db;
+
+        public SongService(SongContext db)
+        {
+            this.db = db;
+        }
+        public SongDto Get(int id)
+        {
+           
+        }
+
+        public IEnumerable<SongDto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
