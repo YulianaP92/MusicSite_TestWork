6f5e7c33ae2798da745a9229c2fd95970eb03ebb
yulia <polushka.yulia@gmail.com>
2019-07-09 13:56:23 +0300

    Start

--- MusicSite/MusicSite.DAL/Properties/AssemblyInfo.cs
+++ MusicSite/MusicSite.DAL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MusicSite")]
+[assembly: AssemblyTitle("MusicSite.DAL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MusicSite")]
+[assembly: AssemblyProduct("MusicSite.DAL")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ed25c48d-a3cc-4786-bf77-2248cc855bff")]
+[assembly: Guid("9c08323e-ac50-481d-a43e-d1ababcf9977")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- MusicSite/MusicSite.WEB/App_Start/RouteConfig.cs
+++ MusicSite/MusicSite.WEB/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace MusicSite
+namespace MusicSite.WEB
 {
     public class RouteConfig
     {
